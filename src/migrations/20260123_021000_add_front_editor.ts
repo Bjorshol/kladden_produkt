5c69a8cbf57889b10683ce1fdeb9e9cf560aa295
Bjorshol <bjorsholreinaas@gmail.com>
2026-01-23 06:51:24 +0100

    repareringsforsøk 1

--- src/migrations/20260123_021000_add_front_editor.ts
+++ src/migrations/20260123_021000_add_front_editor.ts
@@ -2,7 +2,8 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postg
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TABLE "front_editor" (
+CREATE TYPE "enum_front_editor_featured_posts_size" AS ENUM('large', 'small');
+CREATE TABLE "front_editor" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"updated_at" timestamp(3) with time zone,
 	"created_at" timestamp(3) with time zone
@@ -14,7 +15,6 @@ CREATE TABLE "front_editor_featured_posts" (
 	"post_id" integer,
 	"size" enum_front_editor_featured_posts_size DEFAULT 'large'
 );
-CREATE TYPE "enum_front_editor_featured_posts_size" AS ENUM('large', 'small');
 CREATE INDEX "front_editor_featured_posts_order_idx" ON "front_editor_featured_posts" USING btree ("_order");
 CREATE INDEX "front_editor_featured_posts_parent_id_idx" ON "front_editor_featured_posts" USING btree ("_parent_id");
 ALTER TABLE "front_editor_featured_posts" ADD CONSTRAINT "front_editor_featured_posts_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."front_editor"("id") ON DELETE cascade ON UPDATE no action;
@@ -23,7 +23,7 @@ ALTER TABLE "front_editor_featured_posts" ADD CONSTRAINT "front_editor_featured_
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   DROP TABLE "front_editor_featured_posts";
+DROP TABLE "front_editor_featured_posts";
 DROP TABLE "front_editor";
 DROP TYPE "enum_front_editor_featured_posts_size";`)
 }
